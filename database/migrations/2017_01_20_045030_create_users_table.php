5a2d6ba751750974aa04ca87e69a655c8eab3e90
xzjs <2310976219@qq.com>
2017-01-25 11:01:32 +0800

    创建相应的模型以及控制器

--- database/migrations/2017_01_20_045030_create_users_table.php
+++ database/migrations/2017_01_20_045030_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('openid');
+            $table->string('nickname');
+            $table->string('head_img');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('users');
     }
 }
