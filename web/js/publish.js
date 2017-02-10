0703afffe8575695f4ce0a0aaf6d91c30ec07d55
yanlli <sarahzhx@163.com>
2017-02-10 14:04:39 +0800

    提交文章

--- web/js/publish.js
+++ web/js/publish.js
@@ -14,4 +14,45 @@ $(function () {
         var url = window.URL.createObjectURL(this.files.item(0));
         $("#pic_insert_icon3").attr("src", url);
     });
+    $("#showIOSDialog1").click(function () {
+        var id = $.cookie('id');
+        var comment=$('#comment').val();
+        var url=$('#url').val();
+        if (id == null) {
+            window.location.href = '/flwechat/web/index.html';
+        }else if (comment==''||url==''){
+            $('#dialog1').fadeIn(200);
+            $('#sure1').click(function(){$('#dialog1').fadeOut(200)});
+        }else{
+            $('.publish_content').prepend('<textarea name="id" style="display: none">'+id+'</textarea>');
+            var formData=new FormData($('.publish_content')[0]);
+            $.ajax({
+                url:"/flwechat/public/article",
+                type:"post",
+                data:formData,
+                processData: false,
+                contentType: false,
+                success:function(result){
+                    if (result!=0){
+                        console.log(result.id);
+                        console.log(result.topic);
+                        $.cookie('article_id',result.id);
+                        // $('#topic_class').val(result.topic);
+                        $('#dialog2').fadeIn(200);
+                        $('#sure2').on('click',function () {
+                            $('#dialog2').fadeOut(200);
+                        });
+                    }
+                }
+            });
+        }
+
+    });
+    // var $iosDialog1 = $('#iosDialog1');
+    // $('#dialogs').on('click', '.weui-dialog__btn', function () {
+    //     $(this).parents('.js_dialog').fadeOut(200);
+    // });
+    // $('#showIOSDialog1').on('click', function () {
+    //     $iosDialog1.fadeIn(200);
+    // });
 });
