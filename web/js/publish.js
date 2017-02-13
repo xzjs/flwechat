016d54020925cd4d9eb10d8b12a9846b939df935
xzjs <2310976219@qq.com>
2017-02-13 15:18:16 +0800

    修复发布页bug

--- web/js/publish.js
+++ web/js/publish.js
@@ -3,6 +3,8 @@
  */
 $(function () {
     var id = $.cookie('id');
+    var article_id=0;
+
     if (id == null) {
         window.location.href = '/flwechat/web/index.html';
     }
@@ -18,44 +20,50 @@ $(function () {
         var url = window.URL.createObjectURL(this.files.item(0));
         $("#pic_insert_icon3").attr("src", url);
     });
+
+    $('#sure2').on('click', function () {
+        $.post('/flwechat/public/article/add_topic', {
+            article_id: article_id,
+            name: $('#topic').val(),
+            user_id: id
+        }, function (result) {
+            if (result == 'true') {
+                // $('#dialog2').fadeOut(200);
+                window.location.href='my_publish.html';
+            }
+        });
+    });
+
     $("#showIOSDialog1").click(function () {
         var id = $.cookie('id');
-        var comment=$('#comment').val();
-        var url=$('#url').val();
+        var comment = $('#comment').val();
+        var url = $('#url').val();
         if (id == null) {
             window.location.href = '/flwechat/web/index.html';
-        }else if (comment==''||url==''){
+        } else if (comment == '' || url == '') {
             $('#dialog1').fadeIn(200);
-            $('#sure1').click(function(){$('#dialog1').fadeOut(200)});
-        }else{
-            $('.publish_content').prepend('<input name="user_id" type="hidden" value='+id+'>');
-            var formData=new FormData($('.publish_content')[0]);
+            $('#sure1').click(function () {
+                $('#dialog1').fadeOut(200)
+            });
+        } else {
+            $('.publish_content').prepend('<input name="user_id" type="hidden" value=' + id + '>');
+            var formData = new FormData($('.publish_content')[0]);
             $.ajax({
-                url:"/flwechat/public/article",
-                type:"post",
-                data:formData,
+                url: "/flwechat/public/article",
+                type: "post",
+                data: formData,
                 processData: false,
                 contentType: false,
-                dataType:"json",
-                success:function(result){
-                    if (result!=0){
-                        $.cookie('article_id',result.id);
-                        var article_id=result.id;
-                        $('#topic').val('#'+result.topic);
-                        var topic=$('#topic_class').val();
+                dataType: "json",
+                success: function (result) {
+                    if (result != 0) {
+                        $.cookie('article_id', result.id);
+                        article_id = result.id;
+                        $('#topic').val('#' + result.topic);
                         $('#dialog2').fadeIn(200);
-                        $('#sure2').on('click',function () {
-                            $.post('/flwechat/public/article/add_topic',{article_id:article_id,name:topic,user_id:id},function (result) {
-                                if (result=='true'){
-                                    $('#dialog2').fadeOut(200);
-                                    window.location.href='my_publish.html';
-                                }
-                            },'json');
-                        });
                     }
                 }
             });
         }
-
     });
 });
