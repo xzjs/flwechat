7664d7a9827cfef5adb8f39a7f91877f53fdd223
xzjs <2310976219@qq.com>
2017-02-14 11:18:31 +0800

    尝试修复微信用户登录bug

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -12,7 +12,7 @@ class WechatController extends Controller
         Log::info('request arrived');
         $wechat=app('wechat');
         $wechat->server->setMessageHandler(function ($message){
-            return 'http://xzjs.love/flwechat/web/';
+            return 'http://xzjs.love/flwechat/public/';
         });
         Log::info('return response');
         return $wechat->server->serve();
@@ -22,21 +22,28 @@ class WechatController extends Controller
      * 获取用户的信息
      */
     public function getuser(){
-        $user = session('wechat.oauth_user');
-        //var_dump($user);
-        if($user!=null) {
-            $u = User::firstOrNew(['openid' => $user['id']]);
-            $u->nickname = $user["nickname"];
-            $u->head_img = $user['avatar'];
-            $u->follow=0;
-            $u->be_follow=0;
-            $u->save();
+        try {
+            $user = session('wechat.oauth_user');
+            //var_dump($user);
+            if($user!=null) {
+                $u = User::firstOrNew(['openid' => $user['id']]);
+                if($u->nickname==null){
+                    $u->nickname = $user["nickname"];
+                    $u->head_img = $user['avatar'];
+                    $u->follow=0;
+                    $u->be_follow=0;
+                    $u->save();
+                }
 //            echo $u->id;
-            setcookie('id', $u->id, time()+3600,"/");
-            header("Location: /flwechat/web/index.html");
-            exit;
-        }else{
-            echo 0;
+                setcookie('id', $u->id, time()+3600,"/");
+                header("Location: /flwechat/web/index.html");
+                exit;
+            }else{
+                echo 0;
+            }
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
         }
+
     }
 }
