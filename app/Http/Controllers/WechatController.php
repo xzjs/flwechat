b7a74be7ad92804288812bc450c244dfe6947b3a
xzjs <2310976219@qq.com>
2017-02-14 10:39:29 +0800

    修复网址bug

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -12,7 +12,7 @@ class WechatController extends Controller
         Log::info('request arrived');
         $wechat=app('wechat');
         $wechat->server->setMessageHandler(function ($message){
-            return 'http://http://120.27.35.90/flwechat/web/';
+            return 'http://120.27.35.90/flwechat/web/';
         });
         Log::info('return response');
         return $wechat->server->serve();
