f11d366dd6dc8c9a65e17869c9df8b195b954445
xzjs <2310976219@qq.com>
2017-02-28 14:09:55 +0800

    修复wx.config的bug

--- web/js/article_detail.js
+++ web/js/article_detail.js
@@ -54,7 +54,15 @@ function loadMainData() {
             $.post('/flwechat/public/getconfig',
                 {'url': window.location.href},
                 function (config) {
-                    wx.config(config);
+                    console.log(config);
+                    wx.config({
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: config.appId, // 必填，公众号的唯一标识
+                        timestamp: config.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: config.nonceStr, // 必填，生成签名的随机串
+                        signature: config.signature,// 必填，签名，见附录1
+                        jsApiList: config.jsApiList // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    });
                     var icon = '/flwechat/web/images/topic.png';
                     if (result.images.length > 0) {
                         icon = result.images[0].img;
@@ -72,7 +80,7 @@ function loadMainData() {
                             alert('取消分享');
                         }
                     });
-                });
+                },'json');
 
         } else {
             $('#article_content').html('用户已删除该文章');
