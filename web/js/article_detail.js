5f27bb18e45223f64be354d957ebd37df919ac8e
xzjs <2310976219@qq.com>
2017-02-28 14:35:07 +0800

    懒得写

--- web/js/article_detail.js
+++ web/js/article_detail.js
@@ -84,6 +84,22 @@ function loadMainData() {
                             alert('取消分享');
                         }
                     });
+                    wx.onMenuShareAppMessage({
+                        title: desc, // 分享标题
+                        desc: desc, // 分享描述
+                        link: window.location.href, // 分享链接
+                        imgUrl: "http://"+window.location.host+"/flwechat/public/storage/"+icon, // 分享图标
+                        type: '', // 分享类型,music、video或link，不填默认为link
+                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                        success: function () {
+                            // 用户确认分享后执行的回调函数
+                            alert('分享成功');
+                        },
+                        cancel: function () {
+                            // 用户取消分享后执行的回调函数
+                            alert('取消分享');
+                        }
+                    });
                 },'json');
 
         } else {
