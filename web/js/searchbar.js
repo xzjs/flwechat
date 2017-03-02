72e31e8943b7bd73803b94bcf9323d9773654cb5
xzjs <2310976219@qq.com>
2017-03-02 16:46:45 +0800

    去除搜索栏里的form标签

--- web/js/searchbar.js
+++ web/js/searchbar.js
@@ -3,14 +3,13 @@
  */
 $(function () {
     var $searchBar = $('#searchBar'),
-        $searchResult = $('#searchResult'),
         $searchText = $('#searchText'),
         $searchInput = $('#searchInput'),
         $searchClear = $('#searchClear'),
         $searchCancel = $('#searchCancel');
 
     function hideSearchResult() {
-        $searchResult.hide();
+        //$searchResult.hide();
         $searchInput.val('');
     }
 
@@ -28,13 +27,13 @@ $(function () {
         .on('blur', function () {
             if (!this.value.length) cancelSearch();
         })
-        .on('input', function () {
-            if (this.value.length) {
-                $searchResult.show();
-            } else {
-                $searchResult.hide();
-            }
-        })
+        // .on('input', function () {
+        //     if (this.value.length) {
+        //         $searchResult.show();
+        //     } else {
+        //         $searchResult.hide();
+        //     }
+        // })
     ;
     $searchClear.on('click', function () {
         hideSearchResult();
