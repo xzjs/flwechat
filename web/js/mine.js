4d9dba7135da049c8503cb296ee3b3c618c9ddf6
xzjs <2310976219@qq.com>
2017-03-10 10:08:41 +0800

    修复mine页，暂时没有添加点赞和关注功能

--- web/js/mine.js
+++ web/js/mine.js
@@ -27,23 +27,23 @@ $(document).ready(function () {
         $('#follow_num').html(result.follow);
         $('#be_follow_num').html(result.be_follow);
     });
-    $('.messages span').on('click', function () {
-        $(this).addClass('selected').siblings().removeClass('selected');
-        $('.content_box').html('');
-        var html = $(this).html();
-        var url = '';
-        if (html != '发布的文章') {
-            // url = '/flwechat/public/article/comment_articles/' + id;
-            // getArticleList(url);
-            window.location.href = 'mine.html?user_id=' + user_id + '&comment=1';
-        }
-    });
-    $('span').removeClass('selected');
-    if (data['comment'] == 1) {
-        $('#comment').addClass('selected');
-    } else {
-        $('#publish').addClass('selected');
-    }
+    // $('.messages span').on('click', function () {
+    //     $(this).addClass('selected').siblings().removeClass('selected');
+    //     $('.content_box').html('');
+    //     var html = $(this).html();
+    //     var url = '';
+    //     if (html != '发布的文章') {
+    //         // url = '/flwechat/public/article/comment_articles/' + id;
+    //         // getArticleList(url);
+    //         window.location.href = 'mine.html?user_id=' + user_id + '&comment=1';
+    //     }
+    // });
+    // $('span').removeClass('selected');
+    // if (data['comment'] == 1) {
+    //     $('#comment').addClass('selected');
+    // } else {
+    //     $('#publish').addClass('selected');
+    // }
 });
 
 function set_friend(type, show_id) {
@@ -77,3 +77,46 @@ function set_friend(type, show_id) {
         })
     }
 }
+
+var user=GetQueryString('user_id');
+if(user==null){
+    user=user_id;
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        items: [],
+        postData: {
+            page: 0,
+            size: 15,
+            user_id: user
+        }
+    },
+    mounted: function () {
+        this.getArticles(0)
+    },
+    methods: {
+        getArticles: function (comment,event) {
+            var vm = this;
+            vm.postData['comment'] = comment;
+            vm.$http.post('/flwechat/public/article/article_list', vm.postData)
+                .then((response)=> {
+                    console.log(response.data);
+                    vm.items = response.data;
+                    if(response.data.length>0){
+                        vm.show = false;
+                    }else{
+                        vm.show = true;
+                    }
+                }, (response)=> {
+                    console.log(response.data);
+                });
+            if(event!=null){
+                var element=$(event.target);
+                element.addClass('selected').siblings().removeClass('selected');
+            }
+        }
+    }
+})
