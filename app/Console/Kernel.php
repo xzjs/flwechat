b021974071156b09520a34d899ad8d42892a04e8
xzjs <2310976219@qq.com>
2017-03-27 16:24:30 +0800

    添加定时任务调度

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Expand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,36 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            try {
+                $expands=Expand::where('domain','weixin')->where('updated_at','<',time())->get();
+                foreach ($expands as $expand) {
+                    //此处需要图片识别获取url
+                    $ch = curl_init();
+
+                    $data['title'] = $expand->title;
+                    $data['src'] = $expand->src;
+                    curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+                    curl_setopt($ch, CURLOPT_URL, 'http://112.74.36.180:8080/api/updateweixinurl');
+                    curl_setopt($ch, CURLOPT_POST, 1);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_HEADER, false);
+
+                    $return_data = curl_exec($ch);
+                    $log = curl_getinfo($ch);
+                    if ($return_data == false) {
+                        throw new \Exception(curl_error($ch), curl_errno($ch));
+                    }
+                    curl_close($ch);
+                    $url_data = \GuzzleHttp\json_decode($return_data);
+                    $expand->href=$url_data->href;
+                    $expand->save();
+                }
+            } catch (\Exception $exception) {
+                echo $exception->getMessage();
+            }
+        })->hourly()->appendOutputTo('/usr/share/nginx/html/flwechat/public/log');
     }
 
     /**
