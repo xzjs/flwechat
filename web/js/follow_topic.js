5ed1a64652f2b940189afdeac3f086ea6aecad89
yanlli <sarahzhx@163.com>
2017-03-30 14:56:28 +0800

    话题关注样式调整

--- web/js/follow_topic.js
+++ web/js/follow_topic.js
@@ -13,13 +13,15 @@ function get_content() {
             var html = '';
             for (var i = 0; i < result.length; i++) {
                 html += '<div class="user_list">'
-                    + '<a href="index.html?id=' + result[i].id + '">'
+                    + '<div class="head_portrait_box">'
                     + '<img src="images/topic.png" alt="" class="head_portrait"/>'
-                    + '<div>'
+                    +'</div>'
+                    + '<div class="follow_icon_box"><img class="follow_action" src="images/follow3.png" alt="" id="follow_icon" data-id="'+result[i].id+'"></div>'
+                    + '<a href="index.html?id=' + result[i].id + '">'
                     + '<div class="user_name">' + result[i].content + '</div>'
                     + '<!--<p class="new_messages_num">5<span>条新状态</span></p>-->'
-                    + '</div></a>'
-                    + '<div class="follow_icon_box"><img class="follow_action" src="images/follow3.png" alt="" id="follow_icon" data-id="'+result[i].id+'"></div> </div>';
+                    + '</a>'
+                    +'</div>';
             }
             $('#follow_list').html(html);
             set_follow(1);
