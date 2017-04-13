0d61a9b67efd265d48e9fa69071ea9404dbff610
xzjs <2310976219@qq.com>
2017-04-13 18:34:09 +0800

    friend组件添加cell组件

--- project/src/components/Friend.vue
+++ project/src/components/Friend.vue
@@ -1,26 +1,23 @@
 <template>
   <div>
-    <index-list>
+    <index-list :show-indicator="false">
       <index-section index="A">
-        <div>A</div>
-      </index-section>
-      <index-section index="B">
-        <div>A</div>
-      </index-section>
-      <index-section index="C">
-        <div>A</div>
+        <cell title="张艳丽">
+          <img slot="icon" src="http://wx.qlogo.cn/mmopen/lRbhlKQgnGEJfqGPT2OamInAJcgmfY2k5dhTPurSXqW6QlsJkMgTn3kn8bIH4449r8oDCic9dMM7nKBeoGRj3DnTvr8ILQzfy/0" width="30" height="30">
+        </cell>
       </index-section>
     </index-list>
   </div>
 </template>
 
 <script>
-  import { IndexList, IndexSection } from 'mint-ui';
+  import { IndexList, IndexSection ,Cell} from 'mint-ui';
 
   export default{
     components:{
       "index-list":IndexList,
       "index-section":IndexSection,
+      "cell":Cell
     }
   }
 </script>
