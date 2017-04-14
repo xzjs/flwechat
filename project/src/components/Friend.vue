7971eb5c7051a43611a34b1005f62f8842b704b5
xzjs <2310976219@qq.com>
2017-04-14 13:23:17 +0800

    完成好友页组件

--- project/src/components/Friend.vue
+++ project/src/components/Friend.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <index-list :show-indicator="false">
-      <index-section index="A">
-        <cell title="张艳丽">
-          <img slot="icon" src="http://wx.qlogo.cn/mmopen/lRbhlKQgnGEJfqGPT2OamInAJcgmfY2k5dhTPurSXqW6QlsJkMgTn3kn8bIH4449r8oDCic9dMM7nKBeoGRj3DnTvr8ILQzfy/0" width="30" height="30">
+    <index-list>
+      <index-section v-for="(value, key) in map" :index="key" :key="key">
+        <cell v-for="item in value" :title="item.nickname" :key="item.id">
+          <img slot="icon" :src="item.head_img" width="30" height="30">
         </cell>
       </index-section>
     </index-list>
@@ -12,12 +12,77 @@
 
 <script>
   import { IndexList, IndexSection ,Cell} from 'mint-ui';
+  import {mapState} from 'vuex';
+  import axios from 'axios';
 
   export default{
+    data:function () {
+      return{
+        map:{},
+        users:[]
+      }
+    },
+    computed: mapState([
+      'userId'
+    ]),
     components:{
       "index-list":IndexList,
       "index-section":IndexSection,
       "cell":Cell
+    },
+    methods:{
+      parseData:function () {
+        var vm=this;
+        axios.post('/flwechat/public/friend/get_friends',{id:this.userId,type:0})
+                .then(function (response) {
+                  vm.users=response.data;
+                  var firstCharUpper;
+                  var m={};
+                  vm.users.forEach(function(item){
+                    firstCharUpper = vm.getFirstUpperChar(item.nickname);
+                    if (m.hasOwnProperty(firstCharUpper)) {
+                      m[firstCharUpper].push(item);
+                    } else {
+                      m[firstCharUpper]=[item];
+                    }
+                  });
+                  vm.map=m;
+                })
+                .catch(function (response) {
+                  console.log(response.data);
+                })
+      },
+      getFirstUpperChar: function(str){
+        var str = String(str);
+        var c = str[0];
+        if (/[^\u4e00-\u9fa5]/.test(c)) {
+          return c.toUpperCase();
+        }
+        else {
+          return this.chineseToEnglish(c);
+        }
+      },
+      chineseToEnglish: function(c){
+        var idx = -1;
+        var MAP = 'ABCDEFGHJKLMNOPQRSTWXYZ';
+        var boundaryChar = '驁簿錯鵽樲鰒餜靃攟鬠纙鞪黁漚曝裠鶸蜶籜鶩鑂韻糳';
+        if (!String.prototype.localeCompare) {
+          throw Error('String.prototype.localeCompare not supported.');
+        }
+        if (/[^\u4e00-\u9fa5]/.test(c)) {
+          return c;
+        }
+        for (var i = 0; i < boundaryChar.length; i++) {
+          if (boundaryChar[i].localeCompare(c, 'zh-CN-u-co-pinyin') >= 0) {
+            idx = i;
+            break;
+          }
+        }
+        return MAP[idx];
+      },
+    },
+    mounted:function () {
+      this.parseData();
     }
   }
 </script>
