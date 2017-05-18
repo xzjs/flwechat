d96251e66e37f4d700a16e2754be18838d208e7b
xzjs <2310976219@qq.com>
2017-05-18 08:57:23 +0800

    将编译好的文件添加进版本控制里

---
+++ project/dist/static/js/8.3e9ea665c9eed54bf2d0.js
@@ -0,0 +1,2 @@
+webpackJsonp([8,12],{110:function(e,t,n){var o=n(39)(n(189),n(265),null,null);e.exports=o.exports},189:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={methods:{login:function(){localStorage.token=this.$route.params.token,this.$router.push("/")}},created:function(){this.login()}}},265:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement;return(e._self._c||t)("div")},staticRenderFns:[]}}});
+//# sourceMappingURL=8.3e9ea665c9eed54bf2d0.js.map
